60b26b4b116ea70bd96d4660696adc44440f48bb
Golam Alif <71963569+SMGolamAlif@users.noreply.github.com>
2025-07-24 01:42:04 +0600

    deploy error fixed

--- app/root.tsx
+++ app/root.tsx
@@ -7,7 +7,7 @@ import {
   Scripts,
   ScrollRestoration,
 } from "@remix-run/react";
-import tailwindStylesheetUrl from "./tailwind.css";
+import tailwindStylesheetUrl from "./tailwind.css?url";
 
 export const links: LinksFunction = () => [
   { rel: "stylesheet", href: tailwindStylesheetUrl },
